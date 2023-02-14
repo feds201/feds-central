bd28c996743c401a351cd178fb250fd982067b84
sukheshCoding <sukheshkumar.saravanan@gmail.com>
2023-02-13 20:18:58 -0500

    Grabbed From Old App

---
+++ scouting/old-yearly/2023/app/src/main/java/com/feds201/scoutingapp2023/MainActivity.java
@@ -0,0 +1,16 @@
+package com.feds201.scoutingapp2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
