024c2b3281de4815f85af6560e5549928b698351
Nihar Ogale <ogalenihar@gmail.com>
2023-03-18 16:34:58 -0400

    fuck this shit

--- robot/template/src/main/java/frc/robot/subsystems/ClawSubsystem.java
+++ robot/template/src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -1,9 +1,11 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -19,6 +21,12 @@ public class ClawSubsystem extends SubsystemBase{
         m_clawMotor = new TalonFX(ClawConstants.kClawMotorMain);
         m_clawMotor2 = new TalonFX(ClawConstants.kClawMotorFollow);
         ClawConstants.configMotor(m_clawMotor); 
+        ClawConstants.configMotor(m_clawMotor2);
+
+        m_clawMotor2.follow(m_clawMotor);
+        m_clawMotor2.setInverted(TalonFXInvertType.FollowMaster);
+
+        m_clawMotor.setInverted(TalonFXInvertType.CounterClockwise);
     }
 
     public void intakeCone() {
