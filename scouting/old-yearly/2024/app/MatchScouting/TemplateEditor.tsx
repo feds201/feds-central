efbeaf5cc569626871de533a73a14aee45563397
sukheshCoding <sukheshkumar.saravanan@gmail.com>
2024-01-04 19:26:19 -0500

    Template Layout

--- scouting/old-yearly/2024/app/MatchScouting/TemplateEditor.tsx
+++ scouting/old-yearly/2024/app/MatchScouting/TemplateEditor.tsx
@@ -1,6 +1,6 @@
 import React, {useCallback, useMemo, useRef, useState} from "react";
 import {View, StyleSheet, Text, TouchableOpacity} from "react-native";
-import {Item, mapIndexToData} from "../../components/itemType"
+import {Item} from "../../components/itemType"
 import BottomSheet from "@gorhom/bottom-sheet";
 import DraggableFlatList from "react-native-draggable-flatlist/src/components/DraggableFlatList";
 import {componentsView} from "../../components/TemplateComponents";
@@ -9,66 +9,68 @@ import Entypo from "react-native-vector-icons/Entypo";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import Ionicons from "react-native-vector-icons/Ionicons"
 import Feather from "react-native-vector-icons/Feather"
+import {Overlay} from "@rneui/base";
 
-const initialData: Item[] = [{text: "plusminus", key: "0"}];
+const initialData: Item[] = [{text: "", key: ""}];
 
 export default function templateEditor() {
+    const [index, setIndex] = useState(0);
     const [data, setData] = useState(initialData);
-
     const bottomSheetRef = useRef<BottomSheet>(null);
-
-    const snapPoints = useMemo(() => ['10%', '50%'], []);
-
+    const snapPoints = useMemo(() => ['3.5%', '40%'], []);
     const handleSheetChanges = useCallback((index: number) => {
         console.log('handleSheetChanges', index);
     }, []);
 
-    // renders
+    function pushComponent(text: string) {
+        setIndex(index + 1);
+        setData([...data, {text: text, key: index.toString()}]);
+    }
+
     return (
         <View>
-            <DraggableFlatList data={(data)} onDragEnd={({data}) => setData(data)} keyExtractor={(item) => item.key} renderItem={componentsView}/>
-            <View style={styles.container}>
-                <BottomSheet ref={bottomSheetRef} index={1} snapPoints={snapPoints} onChange={handleSheetChanges}>
-                    <View style={styles.contentContainer}>
-                        <TouchableOpacity style={{backgroundColor: "#4287f5", borderRadius: 100, padding: 30, marginLeft:20}}>
-                            <MaterialCommunityIcons name="label" size={35} color="#fff" />
-                        </TouchableOpacity>
-                        <TouchableOpacity style={{backgroundColor: "#4287f5", borderRadius: 200, padding: 30, marginLeft:18}}>
-                            <MaterialCommunityIcons name="plus-minus-variant" size={35} color="#fff" />
-                        </TouchableOpacity>
-                        <TouchableOpacity style={{backgroundColor: "#4287f5", borderRadius: 100, padding: 30, marginLeft:18}}>
-                            <Ionicons name="checkbox-outline" size={35} color="#fff" />
-                        </TouchableOpacity>
-                        <TouchableOpacity style={{backgroundColor: "#4287f5", borderRadius: 100, padding: 30, marginLeft:20, marginTop: 20}}>
-                            <Entypo name="stopwatch" size={35} color="#fff" />
-                        </TouchableOpacity>
-                        <TouchableOpacity style={{backgroundColor: "#4287f5", borderRadius: 100, padding: 30, marginLeft:20, marginTop: 20}}>
-                            <MaterialIcons name="notes" size={35} color="#fff" />
-                        </TouchableOpacity>
-                        <TouchableOpacity style={{backgroundColor: "#4287f5", borderRadius: 100, padding: 30, marginLeft:20, marginTop: 20}}>
-                            <Feather name="sliders" size={35} color="#fff" />
-                        </TouchableOpacity>
-                    </View>
-                </BottomSheet>
+            <View style={{height: "100%"}}>
+                <DraggableFlatList data={(data)} onDragEnd={({data}) => setData(data)} keyExtractor={(item) => item.key} renderItem={componentsView}/>
             </View>
+                <BottomSheet ref={bottomSheetRef} index={1} snapPoints={snapPoints} onChange={handleSheetChanges}>
+                        <View style={styles.contentContainer}>
+                            <TouchableOpacity style={{backgroundColor: "#4287f5", borderRadius: 100, padding: 30, marginLeft:20}} onPress={() => pushComponent("header")}>
+                                <MaterialCommunityIcons name="label" size={35} color="#fff"/>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={{backgroundColor: "#4287f5", borderRadius: 200, padding: 30, marginLeft:18}} onPress={() => pushComponent("plusminus")}>
+                                <MaterialCommunityIcons name="plus-minus-variant" size={35} color="#fff"/>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={{backgroundColor: "#4287f5", borderRadius: 100, padding: 30, marginLeft:18}}>
+                                <Ionicons name="checkbox-outline" size={35} color="#fff" />
+                            </TouchableOpacity>
+                            <TouchableOpacity style={{backgroundColor: "#4287f5", borderRadius: 100, padding: 30, marginLeft:20, marginTop: 20}}>
+                                <Entypo name="stopwatch" size={35} color="#fff" />
+                            </TouchableOpacity>
+                            <TouchableOpacity style={{backgroundColor: "#4287f5", borderRadius: 100, padding: 30, marginLeft:20, marginTop: 20}}>
+                                <MaterialIcons name="notes" size={35} color="#fff" />
+                            </TouchableOpacity>
+                            <TouchableOpacity style={{backgroundColor: "#4287f5", borderRadius: 100, padding: 30, marginLeft:20, marginTop: 20}}>
+                                <Feather name="sliders" size={35} color="#fff" />
+                            </TouchableOpacity>
+                        </View>
+                    </BottomSheet>
         </View>
     );
 }
 
-//<DraggableFlatList data={(hitler)} onDragEnd={({data}) => heilHitler(data)}
-//                                keyExtractor={(item) => item.key} renderItem={componentsView}/>
-
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 605,
-        backgroundColor: "#fffafa",
+        flex: 1,
+        backgroundColor: "#fff",
     },
     contentContainer: {
         backgroundColor: "#fff",
-        flex: 1,
         flexDirection: "row",
         flexWrap: "wrap"
     },
+    footer: {
+        height: 400
+    }
 });
 
 //<Button title={"Reset"} onPress={() => setData(listComponents)}/>
