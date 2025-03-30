6af1d9586280a69c890f8e06cbd810ff38443940
RITESH RAJ AS <riteshrajas.arulselvan@gmail.com>
2025-03-30 18:21:25 -0400

    Add new audio files and configuration for Scout-Ops DataLogger; remove obsolete files

--- scouting/toolchains/test.py
+++
@@ -1,8 +0,0 @@
-import json
-import adit
-
-event_data_summary = adit.StatboticsClient()
-
-with open("event_teams.json", "w") as f:
-    teams = event_data_summary.fetch_teams("2025miket")
-    f.write(json.dumps(teams, indent=4))
