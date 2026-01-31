57b0372864c70deaf446b5304b20dca6c458b7dc
Open Source <oss@github.com>
2026-01-31 00:28:01 -0500

    uhnuonuo Co-authored-by: PhantomPorter <PhantomFool@users.noreply.github.com> Co-authored-by: DalvidLee <DalvidLee@users.noreply.github.com>

--- scouting/android/lib/main.dart
+++ scouting/android/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:battery_plus/battery_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive_flutter/adapters.dart';
@@ -103,15 +102,14 @@ class _MyAppState extends State<MyApp> {
 // Fix the misleadingly named function
 
 bool islightmode() {
-return isDarkMode;
-  
+  return isDarkMode;
 }
 
 void setmode(bool mode) {
   isDarkMode = mode;
 }
 
-void toggle(){
+void toggle() {
   isDarkMode = !isDarkMode;
 }
 
