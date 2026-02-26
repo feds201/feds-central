71d93699dff17dba7afe8c04201090f8df4ae2c9
GunhongMin <kmin7586@rcs-k12.us>
2026-02-25 19:41:48 -0500

    Updated TestLeftTwoPassBump on Pathplanner

---
+++ robot/2026-rebuilt/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026-rebuilt";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1015;
+  public static final String GIT_SHA = "d37a2f2734177f90616a1c3d610b0daa967280af";
+  public static final String GIT_DATE = "2026-02-25 23:30:28 UTC";
+  public static final String GIT_BRANCH = "robot_shooter";
+  public static final String BUILD_DATE = "2026-02-25 23:32:35 UTC";
+  public static final long BUILD_UNIX_TIME = 1772062355437L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
