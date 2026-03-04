abdf178e622a3b1c64ab25434095ec3863f724f8
Clifford St. Clair <156970151+zombieking1555@users.noreply.github.com>
2026-03-04 18:27:12 -0500

    added back auto chooser temp

--- robot/2026-rebuilt/src/main/java/frc/robot/RobotContainer.java
+++ robot/2026-rebuilt/src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,8 @@ import com.pathplanner.lib.auto.NamedCommands;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
