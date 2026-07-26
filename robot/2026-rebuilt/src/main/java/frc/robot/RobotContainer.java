43aba082cf9e83aa6016e13952d97cdb65e3d3d9
roselain3 <roselaine56789@gmail.com>
2026-07-26 12:48:41 -0400

    Gearheads Auto

--- robot/2026-rebuilt/src/main/java/frc/robot/RobotContainer.java
+++ robot/2026-rebuilt/src/main/java/frc/robot/RobotContainer.java
@@ -255,6 +255,8 @@ public class RobotContainer extends ControllerBindings {
       autoChooser.addOption("Comp-LeftSotmMidfieldDoublepass",
           new PathPlannerAuto("Comp-RightSotmMidfieldDoublepass", true));
 
+      autoChooser.addOption("Comp-GearheadsLeft", new PathPlannerAuto("Comp-GearheadsRight", true));
+
 
 
     } catch (Exception e) {
